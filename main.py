9931662593dcd84285a0ddce871adf48fe8a5620
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-09-06 22:40:11 -0700

    Add more stuff

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ import argparse
 import requests
 
 
-api_url = "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
-request = requests.get(api_url)
-quote = request.json()['starWarsQuote']
-
-
 def get_file_size(filename: str) -> int:
+    """Gets file size in bytes"""
     with open(filename, 'rb') as file:
         data = file.read()
-        file.close()
         file_length = len(data)
         return file_length
 
 
 def overwite_file(file_length: int, filename: str, letters: str):
+    """Overwrites each byte with a letter from letters"""
     with open(filename, 'w') as file:
         letter = 0
         for num in range(file_length):
-            if letter == 4:
+            # If the string is at the end, reset index
+            if letter == len(letters):
                 letter = 0
+            # Find the location in the file
             file.seek(num)
+            # Write the letter to the location
             file.write(letters[letter])
             letter += 1
 
 
 def parse() -> list:
+    """Gets arg parameters"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", type=str, help="Set file")
     parser.add_argument("-s", help="Star Wars quote", action='store_true')
@@ -35,6 +35,7 @@ def parse() -> list:
 
 
 def star_wars_quote():
+    """Sends a request to star wars api and gets a quote"""
     url = "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
     request = requests.get(url)
     quote = request.json()['starWarsQuote']
@@ -42,11 +43,14 @@ def star_wars_quote():
 
 
 if __name__ == "__main__":
+    # Runs parse on args
     arguments = parse()
+    # Check for arg param s (star wars)
     if arguments.s:
         letters = star_wars_quote()
     else:
         letters = "fred"
+    # Check for filename
     if arguments.f is not None:
         file_size = get_file_size(arguments.f)
         overwite_file(file_size, arguments.f, letters)
